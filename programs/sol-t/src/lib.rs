5e218718d6e6188333c94cfcffa89a9bd5deca99
Morten Bergseng <bergseng.m@gmail.com>
2022-09-13 14:35:59 +0200

    working on local chain

--- programs/sol-t/src/lib.rs
+++ programs/sol-t/src/lib.rs
@@ -1,18 +1,53 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("DZyZ4HaJQ4kHXoPVFJARnC1gV7NzbujtPJRBu9Hcei3d");
 
 #[program]
 pub mod sol_t {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn send_tweet(
+        send_tweet_ctx: Context<SendTweet>,
+        topic: String,
+        tweet_content: String,
+    ) -> Result<()> {
+        if topic.chars().count() > 50 {
+            return err!(TweetError::TopicTooLong);
+        }
+        if tweet_content.chars().count() > 280 {
+            return err!(TweetError::ContentTooLong);
+        }
+
+        let my_tweet = &mut send_tweet_ctx.accounts.my_tweet;
+        let sender_of_tweet = &send_tweet_ctx.accounts.sender_of_tweet;
+        let clock = Clock::get().unwrap();
+
+        my_tweet.author = *sender_of_tweet.key;
+        my_tweet.timestamp = clock.unix_timestamp;
+        my_tweet.topic = topic;
+        my_tweet.content = tweet_content;
+
         Ok(())
     }
 }
 
+#[error_code]
+pub enum TweetError {
+    #[msg("Tweet topic should be less than 50 chars")]
+    TopicTooLong,
+    #[msg("Theet content should be ledd than 280 chars")]
+    ContentTooLong,
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct SendTweet<'info> {
+    #[account(init, payer=sender_of_tweet, space=Tweet::LEN)]
+    pub my_tweet: Account<'info, Tweet>,
+
+    #[account(mut)]
+    pub sender_of_tweet: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
 
 #[account]
 pub struct Tweet {
@@ -20,4 +55,21 @@ pub struct Tweet {
     pub timestamp: i64,
     pub topic: String,
     pub content: String,
+}
+
+const DISCRIMINATOR_LENGTH: usize = 8;
+const PUBLIC_KEY_LENGTH: usize = 32;
+const TIMESTAMP_LENGTH: usize = 8;
+const STRING_LENGTH_PREFIX: usize = 4; // Size of the string
+const MAX_TOPIC_LENGTH: usize = 50 * 4; // 50 chars max
+const MAX_CONTENT_LENGTH: usize = 280 * 4; // 280 chars max
+
+impl Tweet {
+    const LEN: usize = DISCRIMINATOR_LENGTH
+        + PUBLIC_KEY_LENGTH //author
+        + TIMESTAMP_LENGTH //timestamp
+        + STRING_LENGTH_PREFIX
+        + MAX_CONTENT_LENGTH //content
+        + STRING_LENGTH_PREFIX
+        + MAX_TOPIC_LENGTH; //topic
 }
--- tests/sol-t.ts
+++ tests/sol-t.ts
@@ -1,16 +1,93 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { SolT } from "../target/types/sol_t";
+import * as assert from "assert";
 
 describe("sol-t", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+    anchor.setProvider(anchor.AnchorProvider.env());
+    const program = anchor.workspace.SolT as Program<SolT>;
+    const anchorProvider = program.provider as anchor.AnchorProvider
 
-  const program = anchor.workspace.SolT as Program<SolT>;
+    it("Can send a new tweet", async () => {
+        const tweetKeyPair = anchor.web3.Keypair.generate();
+        
+        await program.methods
+            .sendTweet("My tweet", "The first tweet")
+            .accounts({
+                myTweet: tweetKeyPair.publicKey,
+                senderOfTweet: anchorProvider.wallet.publicKey,
+                systemProgram: anchor.web3.SystemProgram.programId
+            })
+            .signers([tweetKeyPair])
+            .rpc();
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+        const tweetAccount = await program.account.tweet.fetch(tweetKeyPair.publicKey);
+
+        assert.equal(tweetAccount.author.toBase58(), anchorProvider.wallet.publicKey);
+        assert.equal(tweetAccount.topic, "My tweet");
+        assert.equal(tweetAccount.content, "The first tweet");
+        assert.ok(tweetAccount.timestamp);
+
+    })
+
+    it("Can send a new tweet without a topic", async () => {
+        const tweetKeyPair = anchor.web3.Keypair.generate();
+        
+        await program.methods
+            .sendTweet("", "The first tweet")
+            .accounts({
+                myTweet: tweetKeyPair.publicKey,
+                senderOfTweet: anchorProvider.wallet.publicKey,
+                systemProgram: anchor.web3.SystemProgram.programId
+            })
+            .signers([tweetKeyPair])
+            .rpc();
+
+        const tweetAccount = await program.account.tweet.fetch(tweetKeyPair.publicKey);
+
+        assert.equal(tweetAccount.author.toBase58(), anchorProvider.wallet.publicKey);
+        assert.equal(tweetAccount.topic, "");
+        assert.equal(tweetAccount.content, "The first tweet");
+        assert.ok(tweetAccount.timestamp);
+
+    })
+
+    it("Can send a new tweet from a different user", async () => {
+        const otherUser = anchor.web3.Keypair.generate();
+        const tweetKeyPair = anchor.web3.Keypair.generate();
+
+        const signature = await program.provider.connection.requestAirdrop(otherUser.publicKey, 1000000000);
+
+        const latestBlockHash = await program.provider.connection.getLatestBlockhash();
+
+        await program.provider.connection.confirmTransaction({
+            blockhash: latestBlockHash.blockhash,
+            lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+            signature: signature
+        })
+        
+        await program.methods
+            .sendTweet("My tweet", "The first tweet")
+            .accounts({
+                myTweet: tweetKeyPair.publicKey,
+                senderOfTweet: otherUser.publicKey,
+                systemProgram: anchor.web3.SystemProgram.programId
+            })
+            .signers([otherUser, tweetKeyPair])
+            .rpc();
+
+        const tweetAccount = await program.account.tweet.fetch(tweetKeyPair.publicKey);
+
+        assert.equal(tweetAccount.author.toBase58(), otherUser.publicKey.toBase58());
+        assert.equal(tweetAccount.topic, "My tweet");
+        assert.equal(tweetAccount.content, "The first tweet");
+        assert.ok(tweetAccount.timestamp);
+
+    })
+
+    it("can fetch all tweets", async () => {
+        const tweetAccounts = await program.account.tweet.all();
+
+        console.log(tweetAccounts);
+    })
 });
